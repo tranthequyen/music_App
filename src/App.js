2e1ce06760f5e5b62b3ed5deccaff2da7614ab7f
tranQuyen <tranquyen160102@gmail.com>
2023-12-28 13:57:58 +0700

    quyen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 
-import './App.css';
+
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout/></>
   );
 }
 
--- src/layout/AppTopBar.js
+++ src/layout/AppTopBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import "./styles/topbar.scss"
+import { Button } from 'primereact/button';
 function AppTopBar() {
       return (
-            <div className=''>
-                  hi
+            <div className='layout_topbar flex'>
+                <Button label="Đăng Nhập" severity="success" rounded />
+                <Button label="Đăng Ký" severity="success" rounded />
             </div>
       )
 }
